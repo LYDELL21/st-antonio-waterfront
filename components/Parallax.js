ff1651972c2afbf32bb468a998fa947fd1b3af4c
LYDELL21 <123222859+LYDELL21@users.noreply.github.com>
2026-03-10 15:20:19 +0530

    added premium UI upgrades pt2.1

--- components/Parallax.js
+++ components/Parallax.js
@@ -4,7 +4,7 @@ return(
 
 <section
 className="h-[60vh] bg-center bg-cover md:bg-fixed flex items-center justify-center"
-style={{backgroundImage:"url('/images/river.webp')"}}
+style={{backgroundImage:"url('/images/river.jpg')"}}
 >
 
 <div className="bg-black/50 text-white px-10 py-8 rounded-lg backdrop-blur">
